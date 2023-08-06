8f3c528df083779f015d0df0e4f2a705a1f6ca5e
akshaypiranav <akshaypiranavb@gmail.com>
2023-08-06 14:30:19 +0530

    30/07/202

--- app/src/main/java/net/gpaau/MECH_F/mechYear2Sem3.java
+++ app/src/main/java/net/gpaau/MECH_F/mechYear2Sem3.java
@@ -1,12 +1,15 @@
 package net.gpaau.MECH_F;
 
 import android.os.Bundle;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import net.gpaau.R;
 
 public class mechYear2Sem3 extends AppCompatActivity {
+
+	Spinner
 	@Override
 	protected void onCreate(Bundle savedInstance){
 		super.onCreate(savedInstance);
